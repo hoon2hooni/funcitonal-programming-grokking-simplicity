790ca85c5dc41a1d341d698452de87848b8e4a4d
hoon2hooni <sanghoon.engineer@gmail.com>
2022-07-06 18:17:03 +0900

    ch05: seperate responsibility updateShippingIcons

--- ch05/updateShippingIcons.ts
+++ ch05/updateShippingIcons.ts
@@ -1,5 +1,5 @@
-import { Cart } from "types";
-import { getFreeShipping, addItem,makeCartItem } from "ch05";
+import { Cart, Item, Button } from "types";
+import { getFreeShipping, addItem, makeCartItem } from "ch05";
 import { getBuyButtonsDom } from "ch04/getBuyButtonsDom";
 
 /**
@@ -27,11 +27,20 @@ export function updateShippingIcons(cart: Cart) {
   const buyButtons = getBuyButtonsDom();
   for (const button of buyButtons) {
     const { item } = button;
-    const newCart = addItem(cart, makeCartItem(item.name, item.price));
-    if (getFreeShipping(newCart)) {
-      button.showFreeShippingIcon();
-    } else {
-      button.hideFreeShippingIcon();
-    }
+    const hasFreeShipping = getsFreeShippingWithItem(cart, item);
+    setFreeShippingIcon(button, hasFreeShipping);
+  }
+}
+
+function getsFreeShippingWithItem(cart: Cart, item: Item) {
+  const newCart = addItem(cart, makeCartItem(item.name, item.price));
+  return getFreeShipping(newCart);
+}
+
+function setFreeShippingIcon(button: Button, isShown: boolean) {
+  if (isShown) {
+    button.showFreeShippingIcon();
+  } else {
+    button.hideFreeShippingIcon();
   }
 }
